3a06a9b3826259876573945fffe6429c750cd043
Kusum <Kusumlamichhane2015@gmail.com>
2021-09-24 17:37:39 +0545

    UpdateCRUD

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> ObjList = _db.Items;
+            return View(ObjList);
+        }
+    }
+}
